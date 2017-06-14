cf8afa70f3ad1e305b281c2a2a5fc33a4d55d97c
Brian Cloutier <github@briancloutier.com>
2017-06-14 18:09:04 +0300

    Add pg_dist_placement

--- src/backend/distributed/citus--7.0-1--7.0-2.sql
+++ src/backend/distributed/citus--7.0-1--7.0-2.sql
@@ -1,10 +1,5 @@
 /* citus--7.0-1--7.0-2.sql */
 
--- set schema to pg_catalog?
-
--- rename the placementid sequence
-  -- to do this, need to touch many tests as well...
-
 ALTER SEQUENCE pg_catalog.pg_dist_shard_placement_placementid_seq
   RENAME TO pg_dist_placement_placementid_seq;
 
@@ -13,15 +8,22 @@ ALTER TABLE pg_catalog.pg_dist_shard_placement
 
 -- also do something about pg_dist_shard_placement_placementid_index
 
---CREATE TABLE pg_dist_placement (
---  placementid BIGINT NOT NULL default nextval('pg_dist_placement_placementid_seq'::regclass),
---  shardid BIGINT NOT NULL,
---  shardstate INT NOT NULL,
---  shardlength BIGINT NOT NULL,
---  groupid INT NOT NULL
---);
+CREATE TABLE citus.pg_dist_placement (
+  placementid BIGINT NOT NULL default nextval('pg_dist_placement_placementid_seq'::regclass),
+  shardid BIGINT NOT NULL,
+  shardstate INT NOT NULL,
+  shardlength BIGINT NOT NULL,
+  groupid INT NOT NULL
+);
+ALTER TABLE citus.pg_dist_placement SET SCHEMA pg_catalog;
+GRANT SELECT ON pg_catalog.pg_dist_placement TO public;
 
--- fill placement with the old contents
+INSERT INTO pg_catalog.pg_dist_placement
+SELECT placementid, shardid, shardstate, shardlength, node.groupid
+FROM pg_dist_shard_placement placement LEFT JOIN pg_dist_node node ON (
+  -- use a LEFT JOIN so if the node is missing for some reason we error out
+  placement.nodename = node.nodename AND placement.nodeport = node.nodeport
+);
 
 --DROP TABLE pg_dist_shard_placement;
 --CREATE VIEW pg_dist_shard_placement AS
