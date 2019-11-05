fcc48b53010ff350ca759e3861146870418f42a8
Philip Dubé <gunkmute@gmail.com>
2019-11-05 19:23:46 +0000

    Remove serialize/deserialize/combine from aggregate support aggregates
    This punts by not seeking to support parallel queries per shard
    A later PR can bring back this code easily enough. I wasn't able to find a test case
    
    Parallelism on the coordinator will require a bit more effort,
    as we need to replace read_intermediate_result with a relation which supports parallel scan

--- src/backend/distributed/sql/citus--9.1-1--9.1-customagg.sql
+++ src/backend/distributed/sql/citus--9.1-1--9.1-customagg.sql
@@ -1,20 +1,5 @@
 SET search_path = 'citus';
 
-CREATE FUNCTION citus_stype_serialize(internal)
-RETURNS bytea
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE;
-
-CREATE FUNCTION citus_stype_deserialize(bytea, internal)
-RETURNS internal
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE;
-
-CREATE FUNCTION citus_stype_combine(internal, internal)
-RETURNS internal
-AS 'MODULE_PATHNAME'
-LANGUAGE C PARALLEL SAFE;
-
 CREATE FUNCTION worker_partial_agg_sfunc(internal, oid, anyelement)
 RETURNS internal
 AS 'MODULE_PATHNAME'
@@ -37,29 +22,21 @@ LANGUAGE C PARALLEL SAFE;
 
 -- select worker_partial_agg(agg, ...)
 -- equivalent to
--- select serialize_stype(agg_without_ffunc(...))
+-- select to_cstring(agg_without_ffunc(...))
 CREATE AGGREGATE worker_partial_agg(oid, anyelement) (
 	STYPE = internal,
 	SFUNC = worker_partial_agg_sfunc,
-	FINALFUNC = worker_partial_agg_ffunc,
-	COMBINEFUNC = citus_stype_combine,
-	SERIALFUNC = citus_stype_serialize,
-	DESERIALFUNC = citus_stype_deserialize,
-	PARALLEL = SAFE
+	FINALFUNC = worker_partial_agg_ffunc
 );
 
 -- select coord_combine_agg(agg, col)
 -- equivalent to
--- select agg_ffunc(agg_combine(col))
+-- select agg_ffunc(agg_combine(from_cstring(col)))
 CREATE AGGREGATE coord_combine_agg(oid, cstring, anyelement) (
 	STYPE = internal,
 	SFUNC = coord_combine_agg_sfunc,
 	FINALFUNC = coord_combine_agg_ffunc,
-	FINALFUNC_EXTRA,
-	COMBINEFUNC = citus_stype_combine,
-	SERIALFUNC = citus_stype_serialize,
-	DESERIALFUNC = citus_stype_deserialize,
-	PARALLEL = SAFE
+	FINALFUNC_EXTRA
 );
 
 RESET search_path;
--- src/backend/distributed/utils/aggregate_utils.c
+++ src/backend/distributed/utils/aggregate_utils.c
@@ -14,16 +14,10 @@
 #include "miscadmin.h"
 #include "pg_config_manual.h"
 
-PG_FUNCTION_INFO_V1(citus_stype_serialize);
-PG_FUNCTION_INFO_V1(citus_stype_deserialize);
-PG_FUNCTION_INFO_V1(citus_stype_combine);
 PG_FUNCTION_INFO_V1(worker_partial_agg_sfunc);
 PG_FUNCTION_INFO_V1(worker_partial_agg_ffunc);
 PG_FUNCTION_INFO_V1(coord_combine_agg_sfunc);
 PG_FUNCTION_INFO_V1(coord_combine_agg_ffunc);
-
-/* TODO nodeAgg seems to decide to use serial/deserial based on stype == internal */
-/*      Preferably we should match that logic, instead of checking serial/deserial oids */
 
 typedef struct StypeBox
 {
@@ -48,6 +42,9 @@ static void agg_sfunc_handle_transition(StypeBox *box, FunctionCallInfo fcinfo,
 static void agg_sfunc_handle_strict_uninit(StypeBox *box, FunctionCallInfo fcinfo, Datum
 										   value);
 
+/*
+ * get_aggform loads corresponding tuple & Form_pg_aggregate for oid
+ */
 static HeapTuple
 get_aggform(Oid oid, Form_pg_aggregate *form)
 {
@@ -61,6 +58,9 @@ get_aggform(Oid oid, Form_pg_aggregate *form)
 }
 
 
+/*
+ * get_procform loads corresponding tuple & Form_pg_proc for oid
+ */
 static HeapTuple
 get_procform(Oid oid, Form_pg_proc *form)
 {
@@ -74,6 +74,9 @@ get_procform(Oid oid, Form_pg_proc *form)
 }
 
 
+/*
+ * get_typeform loads corresponding tuple & Form_pg_type for oid
+ */
 static HeapTuple
 get_typeform(Oid oid, Form_pg_type *form)
 {
@@ -87,6 +90,9 @@ get_typeform(Oid oid, Form_pg_type *form)
 }
 
 
+/*
+ * pallocInAggContext calls palloc in fcinfo's aggregate context
+ */
 static void *
 pallocInAggContext(FunctionCallInfo fcinfo, size_t size)
 {
@@ -250,236 +256,6 @@ agg_sfunc_handle_strict_uninit(StypeBox *box, FunctionCallInfo fcinfo, Datum val
 
 	box->value_null = false;
 	box->value_init = true;
-}
-
-
-/*
- * (box) -> bytea
- * return bytes(box.agg.oid, box.agg.serial(box.value))
- */
-Datum
-citus_stype_serialize(PG_FUNCTION_ARGS)
-{
-	LOCAL_FCINFO(inner_fcinfo, 1);
-	FmgrInfo infodata;
-	FmgrInfo *info = &infodata;
-	StypeBox *box = (StypeBox *) PG_GETARG_POINTER(0);
-	HeapTuple aggtuple;
-	HeapTuple transtypetuple;
-	Form_pg_aggregate aggform;
-	Form_pg_type transtypeform;
-	bytea *valbytes;
-	bytea *realbytes;
-	Oid serial;
-	Size valbyteslen_exhdr;
-	Size realbyteslen;
-	Datum result;
-
-	aggtuple = get_aggform(box->agg, &aggform);
-	serial = aggform->aggserialfn;
-	ReleaseSysCache(aggtuple);
-
-	if (serial == InvalidOid)
-	{
-		/* TODO do we have to fallback to output/receive if not set? */
-		/* ie is it possible for send/recv to be unset? */
-		/* Answer: yes, but extremely rare */
-		transtypetuple = get_typeform(box->transtype, &transtypeform);
-		serial = transtypeform->typsend;
-		ReleaseSysCache(transtypetuple);
-	}
-
-	Assert(serial != InvalidOid);
-
-	fmgr_info(serial, info);
-	if (info->fn_strict && box->value_null)
-	{
-		valbytes = NULL;
-		valbyteslen_exhdr = 0;
-	}
-	else
-	{
-		InitFunctionCallInfoData(*inner_fcinfo, info, 1, fcinfo->fncollation,
-								 fcinfo->context, fcinfo->resultinfo);
-		fcSetArgExt(inner_fcinfo, 0, box->value, box->value_null);
-		result = FunctionCallInvoke(inner_fcinfo);
-		if (inner_fcinfo->isnull)
-		{
-			valbytes = NULL;
-			valbyteslen_exhdr = 0;
-		}
-		else
-		{
-			valbytes = DatumGetByteaPP(result);
-			valbyteslen_exhdr = VARSIZE(valbytes) - VARHDRSZ;
-		}
-	}
-
-	realbyteslen = VARHDRSZ + valbyteslen_exhdr + sizeof(Oid) + sizeof(bool);
-	realbytes = palloc(realbyteslen);
-	SET_VARSIZE(realbytes, realbyteslen);
-	memcpy(VARDATA(realbytes), &box->agg, sizeof(Oid));
-	memcpy(VARDATA(realbytes) + sizeof(Oid), &box->value_null, sizeof(bool));
-	if (valbytes != NULL)
-	{
-		memcpy(VARDATA(realbytes) + sizeof(Oid) + sizeof(bool),
-			   VARDATA(valbytes),
-			   valbyteslen_exhdr);
-		pfree(valbytes);
-	}
-
-	PG_RETURN_BYTEA_P(realbytes);
-}
-
-
-/*
- * (bytea, internal) -> box
- * box.agg = readagg(bytea)
- * box.value_null = readbool(bytea)
- * if (!box.value_null) box.value = agg.deserial(readrest(bytea))
- * return box
- */
-Datum
-citus_stype_deserialize(PG_FUNCTION_ARGS)
-{
-	StypeBox *box;
-	bytea *bytes = PG_GETARG_BYTEA_PP(0);
-	bytea *inner_bytes;
-	Oid agg;
-	HeapTuple aggtuple;
-	HeapTuple transtypetuple;
-	Form_pg_aggregate aggform;
-	Form_pg_type transtypeform;
-	Oid deserial;
-	Oid ioparam;
-	Oid recv;
-	StringInfoData buf;
-	bool value_null;
-
-	memcpy(&agg, VARDATA(bytes), sizeof(Oid));
-	memcpy(&value_null, VARDATA(bytes) + sizeof(Oid), sizeof(bool));
-
-	box = pallocInAggContext(fcinfo, sizeof(StypeBox));
-	box->agg = agg;
-
-	aggtuple = get_aggform(agg, &aggform);
-	deserial = aggform->aggdeserialfn;
-	box->transtype = aggform->aggtranstype;
-	ReleaseSysCache(aggtuple);
-
-	get_typlenbyval(box->transtype,
-					&box->transtypeLen,
-					&box->transtypeByVal);
-
-	if (value_null)
-	{
-		box->value = (Datum) 0;
-		box->value_null = true;
-		PG_RETURN_POINTER(box);
-	}
-	else if (deserial != InvalidOid)
-	{
-		FmgrInfo fdeserialinfo;
-		LOCAL_FCINFO(fdeserial_callinfo, 2);
-
-		inner_bytes = PG_GETARG_BYTEA_P_SLICE(0, sizeof(Oid), VARSIZE(bytes) -
-											  sizeof(Oid));
-		fmgr_info(deserial, &fdeserialinfo);
-		InitFunctionCallInfoData(*fdeserial_callinfo, &fdeserialinfo, 2,
-								 fcinfo->fncollation, fcinfo->context,
-								 fcinfo->resultinfo);
-		fcSetArg(fdeserial_callinfo, 0, PointerGetDatum(inner_bytes));
-		fcSetArg(fdeserial_callinfo, 1, PG_GETARG_DATUM(1));
-		box->value = FunctionCallInvoke(fdeserial_callinfo);
-		box->value_null = fdeserial_callinfo->isnull;
-	}
-	else
-	{
-		transtypetuple = get_typeform(box->transtype, &transtypeform);
-		ioparam = getTypeIOParam(transtypetuple);
-		recv = transtypeform->typreceive;
-		ReleaseSysCache(transtypetuple);
-
-		initStringInfo(&buf);
-		appendBinaryStringInfo(&buf,
-							   VARDATA(bytes) + sizeof(Oid) + sizeof(bool),
-							   VARSIZE(bytes) - VARHDRSZ - sizeof(Oid) - sizeof(bool));
-
-		box->value = OidReceiveFunctionCall(recv, &buf, ioparam, -1);
-	}
-
-	PG_RETURN_POINTER(box);
-}
-
-
-/*
- * (box1, box2) -> box
- * box1.value = box.agg.combine(box1.value, box2.value)
- * return box
- */
-Datum
-citus_stype_combine(PG_FUNCTION_ARGS)
-{
-	StypeBox *box1 = NULL;
-	StypeBox *box2 = NULL;
-	LOCAL_FCINFO(inner_fcinfo, 2);
-	FmgrInfo info;
-	Oid combine;
-	HeapTuple aggtuple;
-	Form_pg_aggregate aggform;
-
-	if (!PG_ARGISNULL(0))
-	{
-		box1 = (StypeBox *) PG_GETARG_POINTER(0);
-	}
-	if (!PG_ARGISNULL(1))
-	{
-		box2 = (StypeBox *) PG_GETARG_POINTER(1);
-	}
-
-	if (box1 == NULL)
-	{
-		if (box2 == NULL)
-		{
-			PG_RETURN_NULL();
-		}
-
-		box1 = pallocInAggContext(fcinfo, sizeof(StypeBox));
-		memcpy(box1, box2, sizeof(StypeBox));
-		box1->value = (Datum) 0;
-		box1->value_null = true;
-	}
-
-	aggtuple = get_aggform(box1->agg, &aggform);
-	combine = aggform->aggcombinefn;
-	ReleaseSysCache(aggtuple);
-
-	Assert(combine != InvalidOid);
-	fmgr_info(combine, &info);
-
-	if (info.fn_strict)
-	{
-		if (box2->value_null)
-		{
-			PG_RETURN_POINTER(box1);
-		}
-
-		if (box1->value_null)
-		{
-			box1->value = box2->value;
-			box1->value_null = box2->value_null;
-			PG_RETURN_POINTER(box1);
-		}
-	}
-
-	InitFunctionCallInfoData(*inner_fcinfo, &info, 2, fcinfo->fncollation,
-							 fcinfo->context, fcinfo->resultinfo);
-	fcSetArgExt(inner_fcinfo, 0, box1->value, box1->value_null);
-	fcSetArgExt(inner_fcinfo, 1, box2->value, box2->value_null);
-
-	agg_sfunc_handle_transition(box1, fcinfo, inner_fcinfo);
-
-	PG_RETURN_POINTER(box1);
 }
 
 
